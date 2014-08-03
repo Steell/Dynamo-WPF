c570031cd51beca449d18ec43a7cc3f1ce93b12f
Stephen Elliott <steell1890@gmail.com>
2014-08-03 02:49:37 -0400

    more organization

--- DynamoModels/Connector.cs
+++ DynamoModels/Connector.cs
@@ -1,4 +1,29 @@
 ﻿namespace Dynamo.UI.Models
 {
-    public class Connector { }
+    public struct Connector<TNode, TMetaData>
+    {
+        public Connection<TNode, TMetaData> Start, End;
+
+        public override string ToString()
+        {
+            return string.Format("{0}=====>{1}", Start.Node, End.Node);
+        }
+    }
+
+    public struct Connection<TNode, TMetaData>
+    {
+        public TNode Node;
+        public TMetaData PortIndex;
+        public ConnectionType Type;
+
+        public override string ToString()
+        {
+            return string.Format("({0} {1})", Node, Type);//, PortIndex);
+        }
+    }
+
+    public enum ConnectionType
+    {
+        Input, Output
+    }
 }
